b8bb2a9a2d6f41814f091105cbb1e61b013d3def
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-07-29 15:36:23 -0300

    feat: use jsons

---
+++ json/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Dog struct {
+	Name  string `json:"name"`
+	Age   uint   `json:"agr"`
+	Breed string `json:"breed"`
+}
+
+func main() {
+	dog := Dog{"Black", 6, "SRN"}
+	convertedJson, err := json.Marshal(dog)
+	if err != nil {
+		log.Fatal(err)
+	}
+	output := bytes.NewBuffer(convertedJson)
+	fmt.Println(output)
+	fmt.Println("--------------------------------")
+	dog2 := map[string]string{
+		"name":  "Zé",
+		"age":   "2",
+		"breed": "Rotweiller",
+	}
+	dog2Json, err := json.Marshal(dog2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	output2 := bytes.NewBuffer(dog2Json)
+	fmt.Println(output2)
+}
